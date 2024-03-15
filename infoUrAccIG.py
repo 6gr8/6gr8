ddc375c3b9432a8114d151d3c24e5e10f750d187
Mr DeNsor <113623772+6gr8@users.noreply.github.com>
2024-03-15 23:35:30 +0300

    Update infoUrAccIG.py

--- infoUrAccIG.py
+++ infoUrAccIG.py
@@ -106,72 +106,19 @@ req = requests.get(urlchange,data=data,headers=headrsch,cookies=cocke2)
 get_email = req.json()["email_hint"]
 print(Fore.GREEN+"[!] The Email Is "+" : "+get_email)
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
+
 urlchange = 'https://www.instagram.com/accounts/access_tool/former_phones?__a=1'
 
 
 req = requests.get(urlchange,data=data,headers=headrsch,cookies=cocke2)
 print(Fore.GREEN+"[!] The Nums Is "+" : "+req.text)
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
 urlchange = 'https://www.instagram.com/accounts/access_tool/former_usernames?__a=1'
 
 
 req = requests.get(urlchange,data=data,headers=headrsch,cookies=cocke2)
 print(Fore.GREEN+"[!] The Usernames Is "+" : "+req.text)
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
 urlchange = 'https://www.instagram.com/accounts/access_tool/former_bio_texts?__a=1'
 
 
@@ -179,26 +126,6 @@ req = requests.get(urlchange,data=data,headers=headrsch,cookies=cocke2)
 print(Fore.GREEN+"[!] The Bio's Is "+" : "+req.text)
 
 
-
-
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
 urlchange = 'https://www.instagram.com/accounts/access_tool/accounts_you_hide_stories_from?__a=1'
 
 
@@ -206,24 +133,7 @@ req = requests.get(urlchange,data=data,headers=headrsch,cookies=cocke2)
 print(Fore.GREEN+"[!] The Acc's You Hide Is "+" : "+req.text)
 
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
+
 urlchange = 'https://www.instagram.com/accounts/access_tool/former_full_names?__a=1'
 
 
@@ -232,24 +142,6 @@ print(Fore.GREEN+"[!] The Name's Is "+" : "+req.text)
 
 
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
 urlchange = 'https://www.instagram.com/accounts/access_tool/former_emails?__a=1'
 
 
@@ -258,24 +150,7 @@ print(Fore.GREEN+"[!] The Emails Linked Is "+" : "+req.text)
 
 
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
+
 urlchange = 'https://www.instagram.com/accounts/access_tool/accounts_you_blocked?__a=1'
 
 
@@ -284,24 +159,6 @@ print(Fore.GREEN+"[!] The Acc's Blocked Is "+" : "+req.text)
 
 
 
-headrsch = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7,tr;q=0.6',
-            'cache-control': 'max-age=0',
-            'sec-ch-prefers-color-scheme': 'light',
-            'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36',
-            'viewport-width': '1600'
-}
-data = ''
 urlchange = 'https://www.instagram.com/accounts/access_tool/search_history?__a=1'
 
 
